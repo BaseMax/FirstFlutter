5041e1168a0b74cacb62877aa5e8ac40f9acab02
Ritik Harchani <harchani.ritik@gmail.com>
2019-10-06 15:17:51 +0530

    First flutter contribution

--- 0-First-Project/lib/main.dart
+++ 0-First-Project/lib/main.dart
@@ -3,58 +3,65 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-@override
-	Widget build(BuildContext context) {
-		return MaterialApp(
-			title: 'Flutter Demo',
-			theme: ThemeData( primarySwatch: Colors.blue ),
-			home: MyHomePage(title: 'Flutter Demo Home Page'),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
-	MyHomePage({Key key, this.title}) : super(key: key);
-	final String title;
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
 
-	@override
-	_MyHomePageState createState() => _MyHomePageState();
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-	int _counter = 0;
+  int _counter = 0;
 
-	void _incrementCounter() {
-		setState(() {
-			_counter++;
-		});
-	}
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(
-				title: Text(widget.title),
-			),
-			body: Center(
-				child: Column(
-					mainAxisAlignment: MainAxisAlignment.center,
-					children: <Widget>[
-						Text(
-							'You have pushed the button this many times:',
-						),
-						Text(
-							'$_counter',
-							style: Theme.of(context).textTheme.display1,
-						),
-					],
-				),
-			),
-			floatingActionButton: FloatingActionButton(
-				onPressed: _incrementCounter,
-				tooltip: 'Increment',
-				child: Icon(Icons.add),
-			),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Hello BaseMax! It\'s an honour to make PR in your Repo\n ',
+                style: TextStyle(fontSize: 12, color: Colors.red.shade900),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.display1,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
